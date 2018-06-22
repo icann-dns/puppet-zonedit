e272bc0ac5ce6f756e14f0cf9a8c1ef24944f913
John Bond <github@johnbond.org>
2018-06-22 12:50:12 +0100

    mirgate from icann-puppet

---
+++ manifests/init.pp
@@ -0,0 +1,57 @@
+# == Class: zonedit
+#
+class zonedit (
+  String $git_pub_key,
+  String $git_priv_key,
+  String $git_user      = 'dns0ps',
+  String $zones_repo    = 'git@git.dns.icann.org:/zonedit/zones.git',
+  String $zonedit_repo = 'git@git.dns.icann.org:zonedit/zonedit.git',
+) {
+
+  ensure_packages(['bind9utils', 'git'])
+  python::pip {'GitPython':
+    ensure   => '2.0.5',
+  }
+  ssh_authorized_key { 'git@zonedit.dns.icann.org':
+    user => $git_user,
+    type => 'ssh-rsa',
+    key  => $git_pub_key,
+  }
+  file {'/var/git_repos':
+    ensure => directory,
+    owner  => $git_user,
+  }
+  file {'/etc/bash_completion.d/zonedit':
+    ensure => present,
+    source => 'puppet:///modules/zonedit/etc/bash_completion.d/zonedit',
+  }
+  file{"/home/${git_user}/.ssh/id_rsa":
+    ensure  => file,
+    owner   => $git_user,
+    group   => $git_user,
+    mode    => '0600',
+    content => $git_priv_key,
+  }
+  vcsrepo {'/var/git_repos/zones':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => $zones_repo,
+    user     => $git_user,
+    require  => [
+      File['/var/git_repos'],
+      File["/home/${git_user}/.ssh/id_rsa"],
+    ],
+  }
+  vcsrepo { '/var/git_repos/zonedit':
+    ensure   => latest,
+    provider => git,
+    revision => master,
+    user     => $git_user,
+    source   => $zonedit_repo,
+    require  => File['/var/git_repos'],
+  } -> file { '/usr/local/bin/zonedit':
+    ensure => link,
+    target => '/var/git_repos/zonedit/zonedit.py',
+  }
+}
---
+++ spec/classes/zonedit_spec.rb
@@ -0,0 +1,211 @@
+require 'spec_helper'
+
+describe 'zonedit' do
+  let(:node) { 'zonedit.example.com' }
+  let(:params) do
+    {
+      git_pub_key: 'PUPLIC_KEY',
+      git_priv_key: 'PRIVATE_KEY',
+      # git_user: "dns0ps",
+      # zones_repo: "git@git.dns.icann.org:/zonedit/zones.git",
+      # zonedit_repo: "git@git.dns.icann.org:zonedit/zonedit.git",
+    }
+  end
+
+  # Puppet::Util::Log.level = :debug
+  # Puppet::Util::Log.newdestination(:console)
+  # This will need to get moved
+  # it { pp catalogue.resources }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      describe 'check default config' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_python__pip('GitPython').with_ensure('2.0.5') }
+        it do
+          is_expected.to contain_ssh_authorized_key(
+            'git@zonedit.dns.icann.org',
+          ).with(
+            user: 'dns0ps',
+            type: 'ssh-rsa',
+            key: 'PUPLIC_KEY',
+          )
+        end
+        it do
+          is_expected.to contain_file('/var/git_repos').with(
+            ensure: 'directory',
+            owner: 'dns0ps',
+          )
+        end
+        it do
+          is_expected.to contain_file('/etc/bash_completion.d/zonedit').with(
+            ensure: 'present',
+            source: 'puppet:///modules/zonedit/etc/bash_completion.d/zonedit',
+          )
+        end
+        it do
+          is_expected.to contain_file('/home/dns0ps/.ssh/id_rsa').with(
+            ensure: 'file',
+            owner: 'dns0ps',
+            group: 'dns0ps',
+            mode: '0600',
+            content: 'PRIVATE_KEY',
+          )
+        end
+        it do
+          is_expected.to contain_vcsrepo('/var/git_repos/zones').with(
+            ensure: 'latest',
+            provider: 'git',
+            revision: 'master',
+            source: 'git@git.dns.icann.org:/zonedit/zones.git',
+            user: 'dns0ps',
+            require: ['File[/var/git_repos]', 'File[/home/dns0ps/.ssh/id_rsa]'],
+          )
+        end
+        it do
+          is_expected.to contain_vcsrepo('/var/git_repos/zonedit').with(
+            ensure: 'latest',
+            provider: 'git',
+            revision: 'master',
+            user: 'dns0ps',
+            source: 'git@git.dns.icann.org:zonedit/zonedit.git',
+            require: 'File[/var/git_repos]',
+          )
+        end
+        it do
+          is_expected.to contain_file('/usr/local/bin/zonedit').with(
+            ensure: 'link',
+            target: '/var/git_repos/zonedit/zonedit.py',
+          )
+        end
+      end
+      describe 'Change Defaults' do
+        context 'git_pub_key' do
+          before(:each) { params.merge!(git_pub_key: 'FOOBAR') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_ssh_authorized_key(
+              'git@zonedit.dns.icann.org',
+            ).with(
+              user: 'dns0ps',
+              type: 'ssh-rsa',
+              key: 'FOOBAR',
+            )
+          end
+        end
+        context 'git_priv_key' do
+          before(:each) { params.merge!(git_priv_key: 'FOOBAR') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file('/home/dns0ps/.ssh/id_rsa').with(
+              ensure: 'file',
+              owner: 'dns0ps',
+              group: 'dns0ps',
+              mode: '0600',
+              content: 'FOOBAR',
+            )
+          end
+        end
+        context 'git_user' do
+          before(:each) { params.merge!(git_user: 'foobar') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_ssh_authorized_key(
+              'git@zonedit.dns.icann.org',
+            ).with(
+              user: 'foobar',
+              type: 'ssh-rsa',
+              key: 'PUPLIC_KEY',
+            )
+          end
+          it do
+            is_expected.to contain_file('/var/git_repos').with(
+              ensure: 'directory',
+              owner: 'foobar',
+            )
+          end
+          it do
+            is_expected.to contain_file('/home/foobar/.ssh/id_rsa').with(
+              ensure: 'file',
+              owner: 'foobar',
+              group: 'foobar',
+              mode: '0600',
+              content: 'PRIVATE_KEY',
+            )
+          end
+          it do
+            is_expected.to contain_vcsrepo('/var/git_repos/zones').with(
+              ensure: 'latest',
+              provider: 'git',
+              revision: 'master',
+              source: 'git@git.dns.icann.org:/zonedit/zones.git',
+              user: 'foobar',
+              require: ['File[/var/git_repos]', 'File[/home/foobar/.ssh/id_rsa]'],
+            )
+          end
+          it do
+            is_expected.to contain_vcsrepo('/var/git_repos/zonedit').with(
+              ensure: 'latest',
+              provider: 'git',
+              revision: 'master',
+              user: 'foobar',
+              source: 'git@git.dns.icann.org:zonedit/zonedit.git',
+              require: 'File[/var/git_repos]',
+            )
+          end
+        end
+        context 'zones_repo' do
+          before(:each) { params.merge!(zones_repo: 'foobar') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_vcsrepo('/var/git_repos/zones').with(
+              ensure: 'latest',
+              provider: 'git',
+              revision: 'master',
+              source: 'foobar',
+              user: 'dns0ps',
+              require: ['File[/var/git_repos]', 'File[/home/dns0ps/.ssh/id_rsa]'],
+            )
+          end
+        end
+        context 'zonedit_repo' do
+          before(:each) { params.merge!(zonedit_repo: 'foobar') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_vcsrepo('/var/git_repos/zonedit').with(
+              ensure: 'latest',
+              provider: 'git',
+              revision: 'master',
+              user: 'dns0ps',
+              source: 'foobar',
+              require: 'File[/var/git_repos]',
+            )
+          end
+        end
+      end
+      describe 'check bad type' do
+        context 'git_pub_key' do
+          before(:each) { params.merge!(git_pub_key: true) }
+          it { is_expected.to raise_error(Puppet::Error) }
+        end
+        context 'git_priv_key' do
+          before(:each) { params.merge!(git_priv_key: true) }
+          it { is_expected.to raise_error(Puppet::Error) }
+        end
+        context 'git_user' do
+          before(:each) { params.merge!(git_user: true) }
+          it { is_expected.to raise_error(Puppet::Error) }
+        end
+        context 'zones_repo' do
+          before(:each) { params.merge!(zones_repo: true) }
+          it { is_expected.to raise_error(Puppet::Error) }
+        end
+        context 'zonedit_repo' do
+          before(:each) { params.merge!(zonedit_repo: true) }
+          it { is_expected.to raise_error(Puppet::Error) }
+        end
+      end
+    end
+  end
+end
